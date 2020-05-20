c25708dc0d1c2614d5db2db8a06b60fc6d2ea5f6
Samuel Pinillos <samuelpinillos@Samuels-MacBook-Air.local>
2020-05-20 10:27:38 -0500

    Nothing

--- assets/js/application.js
+++ assets/js/application.js
@@ -4,14 +4,13 @@ require("@fortawesome/fontawesome-free/js/all.js");
 
 $(() => {
     const saveCampaignBtn = document.getElementById("saveCampaign")
-
     $(saveCampaignBtn).click(function(){
         const checkboxs = $('input[type="checkbox"]');
         checkboxs.each(function(){
             if(!$(this).is(":checked")) {
                 $(this).parent().parent().next().remove();     
             }
-        });
+        });   
     });
 
     $("#search").keyup(function() {
